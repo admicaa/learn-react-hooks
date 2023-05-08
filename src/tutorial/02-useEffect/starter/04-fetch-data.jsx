19df652e13f09a0d946ea596246bcd05ebee742e
admica <almestaadmica@gmail.com>
2023-05-09 00:47:50 +0300

    FETCH INSIDE USE EFFECT

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,34 @@
-const url = 'https://api.github.com/users';
+import { useEffect, useState } from "react";
+
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+  const fetchUsers = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setUsers(data);
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+  return (
+    <section>
+      <h3>Github Users</h3>
+      <ul className="users">
+        {users.map((user) => (
+          <li key={user.id}>
+            <img src={user.avatar_url} alt={user.login} />
+            <div>
+              <h5>{user.login}</h5>
+              <a href={user.html_url} target="_blank">
+                Profile
+              </a>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 export default FetchData;
