f60e0214445fefd85ecdc8c882788e8f6658be61
admica <almestaadmica@gmail.com>
2023-05-09 01:14:35 +0300

    CHANGES SIMPLE PAGINATION

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -4,30 +4,64 @@ const url = "https://api.github.com/users";
 
 const FetchData = () => {
   const [users, setUsers] = useState([]);
+  const [perPage, setPerPage] = useState(10);
+  const [page, setPage] = useState(0);
   const fetchUsers = async () => {
-    const response = await fetch(url);
+    setUsers([]);
+    const response = await fetch(`${url}?per_page=${perPage}&since=${page}`);
     const data = await response.json();
-    setUsers(data);
+
+    setTimeout(() => {
+      setUsers(data);
+    }, 3000);
   };
+
+  const prevPage = () => {
+    var since = 0;
+    since = users[0].id - perPage;
+    since = since > 0 ? since : 0;
+    setPage(since);
+  };
+
+  const nextPage = () => {
+    var since = 0;
+    since = users[users.length - 1].id;
+    setPage(since);
+  };
+
   useEffect(() => {
     fetchUsers();
-  }, []);
+  }, [perPage, page]);
   return (
     <section>
       <h3>Github Users</h3>
-      <ul className="users">
-        {users.map((user) => (
-          <li key={user.id}>
-            <img src={user.avatar_url} alt={user.login} />
-            <div>
-              <h5>{user.login}</h5>
-              <a href={user.html_url} target="_blank">
-                Profile
-              </a>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {users.length ? (
+        <>
+          <ul className="users">
+            {users.map((user) => (
+              <li key={user.id}>
+                <img src={user.avatar_url} alt={user.login} />
+                <div>
+                  <h5>{user.login}</h5>
+                  <a href={user.html_url} target="_blank">
+                    Profile
+                  </a>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <button className="btn" onClick={prevPage}>
+              Prev Page
+            </button>
+            <button className="btn" onClick={nextPage}>
+              Nex Page
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className="loading"></div>
+      )}
     </section>
   );
 };
