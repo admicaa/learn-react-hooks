ca904ec161edef580e484c9333749823577714bc
admica <almestaadmica@gmail.com>
2023-05-09 01:17:41 +0300

    CONDITIONAL RENDERING

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from "./tutorial/02-useEffect/starter/04-fetch-data";
-import Final from "./tutorial/02-useEffect/final/04-fetch-data";
+import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
+import Final from "./tutorial/02-useEffect/final/05-cleanup-function";
 function App() {
   return (
     <div className="container">
--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <section>
+      <button className="btn" onClick={() => setShow(!show)}>
+        Toggle Component
+      </button>
+      {show && (
+        <>
+          <h2>Hello There</h2>
+        </>
+      )}
+    </section>
+  );
 };
 
 export default CleanupFunction;
