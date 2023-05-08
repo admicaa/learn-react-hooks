7dddde68870ce78c19586fabf2acbadeeed1c637
admica <almestaadmica@gmail.com>
2023-05-09 02:32:02 +0300

    VANILLA

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Starter from "./tutorial/02-useEffect/starter/04-fetch-data";
-import Final from "./tutorial/02-useEffect/final/04-fetch-data";
+import Starter from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview";
+import Final from "./tutorial/03-conditional-rendering/final/04-short-circuit-overview";
 function App() {
   return (
     <div className="container">
       <Starter />
-      {/* <Final /> */}
+      <Final />
     </div>
   );
 }
--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,15 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [name, setName] = useState("Hello");
+  const [text, setText] = useState("");
+  return (
+    <>
+      <h4>False Or : {text || "Hello world"}</h4>
+      <h4>False And : {text && "Hello world"}</h4>
+      <h4>Truthy Or : {name || "Hello world"}</h4>
+      <h4>Truthy And : {name && "Hello world"}</h4>
+    </>
+  );
 };
 export default ShortCircuitOverview;
