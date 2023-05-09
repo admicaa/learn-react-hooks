9673e2da3b929bc3b1c359100779952f09ea3990
admica <almestaadmica@gmail.com>
2023-05-09 03:58:32 +0300

    FORM SUBMIT

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,21 +1,72 @@
+import { useState } from "react";
+import { data } from "../../../data";
 const UserChallenge = () => {
+  const [users, setUsers] = useState(data);
+  const [name, setName] = useState("");
+  const addUser = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      return;
+    }
+    const newUsers = [
+      ...users,
+      {
+        id: Math.random(),
+        name: name,
+      },
+    ];
+    setUsers(newUsers);
+    setName("");
+  };
+  const removeUser = (id) => {
+    const newUsers = users.filter((user) => user.id !== id);
+    setUsers(newUsers);
+  };
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={addUser}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="form-input"
+            id="name"
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
-      {/* render users below */}
+      <UsersList users={users} removeUser={removeUser} />
     </div>
   );
 };
 export default UserChallenge;
+
+function UsersList({ users, removeUser }) {
+  return (
+    <>
+      <h2>Users</h2>
+      {users.map((user) => (
+        <div key={user.id}>
+          <h4>{user.name}</h4>
+          <button
+            className="btn"
+            onClick={() => {
+              removeUser(user.id);
+            }}
+          >
+            Remove
+          </button>
+        </div>
+      ))}
+    </>
+  );
+}
